2611edfd07e6f23cf6191539b61ef6006d3db6e7
joaolrc5 <jc@ccode.pt>
2021-02-27 20:26:55 +0000

    added first api endpoint and gitignore

---
+++ ReactDjangoTutorial/music_controller/api/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
+
+#.ListApiView -> list DB entries
+class RoomView(generics.CreateAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
